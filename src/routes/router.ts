57df67afcaf2af1b39bd6315ac51716fcd0cbd35
waqas-shabbir-9089 <waqas.shabbir@codingcops.com>
2025-01-30 19:41:05 +0500

    test

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => res.json({ message: "Hello from Docker waqasshabbir3 🎉" }));
+router.get("/", (req, res) => res.json({ message: "Hello from Docker Cathy Heaven 🎉" }));
 
 router.get("/health", (req, res) => {
   res.status(200).json({ message: "Everything is good here 👀" });
