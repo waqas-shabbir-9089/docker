5eb75eac1f3733cb639e3a482181dc948d687458
waqas-shabbir-9089 <waqas.shabbir@codingcops.com>
2025-01-30 18:55:26 +0500

    second commit

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => res.json({ message: "Hello from Docker v3 🎉" }));
+router.get("/", (req, res) => res.json({ message: "Hello from Docker w3 🎉" }));
 
 router.get("/health", (req, res) => {
   throw new Error("Internal Server Error");
